b7b0f2b377ed9787e11619b9c5cd1ebac0197b11
Sajan Bhosale <83913731+sajanbhosale05@users.noreply.github.com>
2023-02-28 12:13:37 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React, { useRef, useState } from 'react'
+import '../styles/App.css';
+const App = () => {
+  const startTime = useRef(0);
+  const intervalRef = useRef(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [laps, setLaps] = useState([]);
+
+  return (
+    <div id="main">
+      <section>
+        <h1 className='seconds-elapsed'>Stopwatch Time</h1>
+        <section className='buttons'>
+          <button className="start-btn">START</button>
+          <button className="stop-btn">STOP</button>
+          <button className="lap-btn">LAP</button>
+          <button className="reset-btn">RESET</button>
+        </section>
+      </section>
+      <section className='lap-section'>
+        <h2>Laps</h2>
+        <section className='laps'>
+          <p>lap</p>
+          <p>lap</p>
+          <p>lap</p>
+        </section>
+      </section>
+    </div>
+  )
+}
+
+
+export default App;
