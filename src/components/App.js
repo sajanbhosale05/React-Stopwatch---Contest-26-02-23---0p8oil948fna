7c7177edefe2a07e0f738da7139ea0d8d559c696
Sajan Bhosale <83913731+sajanbhosale05@users.noreply.github.com>
2023-02-28 12:16:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,45 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState,useEffect } from 'react'
 import '../styles/App.css';
 const App = () => {
   const startTime = useRef(0);
   const intervalRef = useRef(0);
   const [currentTime, setCurrentTime] = useState(0);
   const [laps, setLaps] = useState([]);
-
+  const [running, setRunning] = useState(false);
+  const [lapvisi, setlapvisi] = useState(false);
+  useEffect(() => {
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setCurrentTime((prevTime) => prevTime + .010);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+    
+  function handleLap(){
+    setLaps([...laps,currentTime])
+    setlapvisi(true)
+  }
   return (
     <div id="main">
       <section>
-        <h1 className='seconds-elapsed'>Stopwatch Time</h1>
+        <h1 className='seconds-elapsed'>{currentTime.toFixed(3)}</h1>
         <section className='buttons'>
-          <button className="start-btn">START</button>
-          <button className="stop-btn">STOP</button>
-          <button className="lap-btn">LAP</button>
-          <button className="reset-btn">RESET</button>
+          <button className="start-btn" onClick={()=>{setRunning(true)}}>START</button>
+          <button className="stop-btn" onClick={()=>{setRunning(false)}}>STOP</button>
+          <button className="lap-btn" onClick={handleLap}>LAP</button>
+          <button className="reset-btn" onClick={()=>{setRunning(false) ,setCurrentTime(0),setlapvisi(false),setLaps([])}}>RESET</button>
         </section>
       </section>
-      <section className='lap-section'>
+      {lapvisi&&<section className='lap-section'>
         <h2>Laps</h2>
         <section className='laps'>
-          <p>lap</p>
-          <p>lap</p>
-          <p>lap</p>
+          {laps.map((item)=> <p>{item.toFixed(3)}</p>)}
         </section>
-      </section>
+      </section>}
     </div>
   )
 }
